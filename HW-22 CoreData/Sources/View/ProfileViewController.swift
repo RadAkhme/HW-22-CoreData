702bab2b12c31b325ea9eba54e44aaa95d5a1b1c
Radik Akhmetzianov <109820745+RadAkhme@users.noreply.github.com>
2022-11-16 20:09:04 +0300

    Add ProfileViewController file

---
+++ HW-22 CoreData/Sources/View/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  HW-22 CoreData
+//
+//  Created by Радик Ахметзянов on 16.11.2022.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
