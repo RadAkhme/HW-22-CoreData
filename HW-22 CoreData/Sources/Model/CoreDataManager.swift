fd2912ddad308fc2f22ea40e4a250d2624eb382c
Radik Akhmetzianov <109820745+RadAkhme@users.noreply.github.com>
2022-11-19 22:03:30 +0300

    Add CoreData

---
+++ HW-22 CoreData/Sources/Model/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  HW-22 CoreData
+//
+//  Created by Радик Ахметзянов on 19.11.2022.
+//
+
+import Foundation
---
+++ HW-22 CoreData/Sources/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  HW-22 CoreData
+//
+//  Created by Радик Ахметзянов on 16.11.2022.
+//
+
+import Foundation
