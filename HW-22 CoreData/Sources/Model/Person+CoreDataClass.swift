6d4420e2af14c1f15556cce5e03ea23d6561fe65
Radik Akhmetzianov <109820745+RadAkhme@users.noreply.github.com>
2023-02-26 22:21:18 +0300

    Model

---
+++ HW-22 CoreData/Sources/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  HW-22 CoreData
+//
+//  Created by Радик Ахметзянов on 10.01.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
